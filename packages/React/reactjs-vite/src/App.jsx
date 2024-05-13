ccdd551be27f9a5a8a228d56f83264a83e60b57a
1391020381 <1391020381@qq.com>
2024-05-13 17:58:06 +0800

    feat: reactjs-vite antd

--- packages/React/reactjs-vite/src/App.jsx
+++ packages/React/reactjs-vite/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 
+import { Button } from './components/user/Button'
+
 function App() {
   const [count, setCount] = useState(0)
-
+  const clickFuc = ()=>{
+    console.log('23234234')
+    setCount((count) => count + 1)
+  }
   return (
     <div>
-      <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <Button text={'count is '} count={count}  fuc ={clickFuc}>
+          
+        </Button>
     </div>
   )
 }
--- packages/React/reactjs-vite/src/components/user/Button.jsx
+++ packages/React/reactjs-vite/src/components/user/Button.jsx
@@ -1,7 +1,9 @@
+// import { useNode } from '@craftjs/core'
+import { Button as AntdButton  } from 'antd'
 
-
-export const Button = ({text})=>{
+export const Button = ({text,count,fuc})=>{
+    console.log('Button:',text,count,fuc)
     return (
-        <button>{text}</button>
+        <AntdButton type="primary" onClick={fuc}>{text}{count}</AntdButton>
     )
 }
