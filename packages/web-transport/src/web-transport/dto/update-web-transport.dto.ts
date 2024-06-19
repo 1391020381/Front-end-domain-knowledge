a3fbb22ec0c87d756d194916dd285ad7520f3d0f
1391020381 <1391020381@qq.com>
2024-06-19 14:06:34 +0800

    feat: update

--- packages/web-transport/src/web-transport/dto/update-web-transport.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateWebTransportDto } from './create-web-transport.dto';
-
-export class UpdateWebTransportDto extends PartialType(CreateWebTransportDto) {}
--- packages/web-transport/src/web-transport/web-transport.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WebTransportService } from './web-transport.service';
-import { WebTransportController } from './web-transport.controller';
-
-@Module({
-  controllers: [WebTransportController],
-  providers: [WebTransportService],
-})
-export class WebTransportModule {}
